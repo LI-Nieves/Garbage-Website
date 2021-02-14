50b6e667cc229874ff4a9b22dfd7898ded3374ef
Lana Nieves <noneofyourbeeswax55@gmail.com>
2021-02-13 19:51:00 -0700

    draggy bois

--- testApp/urls.py
+++ testApp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view1, name = 'view1')
+    path('', views.bananaDrag, name = 'bananaDrag')
 ]
--- testApp/views.py
+++ testApp/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from json import dumps
 
 # Create your views here.
 
-def view1(request):
-    return HttpResponse("ur mom")
+def bananaDrag(request):
+    idk = {'a': 'b'}
+    dataJSON = dumps(idk)
+    return render(request, 'banana.html', {'data': dataJSON})
+
+""" def opposites(request): 
+    # create data dictionary 
+    data = [ 
+        ["Laugh", "Cry"], 
+        ["Even", "Odd"], 
+        ["Hot", "Cold"], 
+        ["Light", "Dark"], 
+        ["Opposite", "Same"], 
+        ["Far", "Near"], 
+        ["Give", "Take"], 
+        ["Night", "Day"], 
+        ["Import", "Export"], 
+        ["Hard", "Easy"], 
+        ["Never", "Always"], 
+        ["Late", "Early"], 
+        ["Less", "More"], 
+        ["Male", "Female"], 
+        ["Happiness", "Sadness"], 
+        ["Fast", "Slow"], 
+        ["Old", "Young"], 
+        ["Boy", "Girl"], 
+        ["Up", "Down"], 
+        ["Left", "Right"], 
+        ["Rich", "Poor"], 
+        ["Love", "Hate"], 
+        ["Inside", "Outside"], 
+        ["Bad", "Good"], 
+        ["Short", "Tall"], 
+    ] 
+    data = dumps(data) 
+    return render(request, "opposites.html", {"data": data})  """
